3a91f47c815902a45ad46387652750becacd2832
specifiedUserDoesNotExist <sammael.apoliom@hotmail.com>
2022-05-07 20:27:01 -0300

    Create 001.f95

---
+++ planoInternet/001.f95
@@ -0,0 +1,27 @@
+program main
+	implicit none
+	
+	! cota mensal
+	integer :: X=0
+	!numero meses
+	integer :: N
+	!total megas
+	integer :: totalMega = 0
+	
+	integer :: i, temp
+	
+	open(1, file='planoInternet.txt')
+	
+	read(1, *) X
+	read(1, *) N
+	
+	do i = 1, N
+		read(1, *) temp
+		totalMega = totalMega + X - temp	
+	end do
+	close(1)
+	
+	totalMega = totalMega + X
+	
+	print*, totalMega
+end program
